5f10b3c6871a5b83286ad94de464df5f64f9a39c
Arpad Ryszka <arpad.ryszka@gmail.com>
2017-09-20 21:24:57 +0200

    enable base templates

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const http = require("http")
+const path = require("path")
 const Tailor = require("node-tailor")
-const tailor = new Tailor({})
+const fetchTemplateFromFs = require("node-tailor/lib/fetch-template")
+
+const templatePath = precess.env.TEMPLATE_PATH || path.join(process.cwd(), "templates")
+const baseTemplate = (function () {
+	if (process.env.BASE_TEMPLATE) {
+		return require(process.env.BASE_TEMPLATE)
+	}
+})()
+const fetchTemplate = fetchTemplateFromFs(templatePath, baseTemplate)
+
+const tailor = new Tailor({fetchTemplate})
 const server = http.createServer(tailor.requestHandler)
 server.listen(process.env.PORT || 8001)
